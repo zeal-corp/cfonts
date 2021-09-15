de717061ec074c3289383364bf9a7514a389b4e5
Harris Weeks <harris@joinpuzzl.com>
2021-09-15 12:17:32 -0700

    fix: make it all terrible but work

--- src/GetFont.js
+++ src/GetFont.js
@@ -13,10 +13,23 @@
  *
  **************************************************************************************************************************************************************/
 
-'use strict';
+"use strict";
 
-const { Debugging } = require('./Debugging.js');
+const { Debugging } = require("./Debugging.js");
+const d3 = require(`../fonts/3d.json`);
+const block = require(`../fonts/block.json`);
+const chrome = require(`../fonts/chrome.json`);
+const grid = require(`../fonts/grid.json`);
+const huge = require(`../fonts/huge.json`);
+const pallet = require(`../fonts/pallet.json`);
+const shade = require(`../fonts/shade.json`);
+const simple = require(`../fonts/simple.json`);
+const simple3d = require(`../fonts/simple3d.json`);
+const simpleBlock = require(`../fonts/simpleBlock.json`);
+const slick = require(`../fonts/slick.json`);
+const tiny = require(`../fonts/tiny.json`);
 
+const FONT_HOLSTER = { "3d": d3, block, chrome, grid, huge, pallet, shade, simple, simple3d, simpleBlock, slick, tiny };
 
 /**
  * Get a selected JSON font-file object
@@ -25,24 +38,21 @@ const { Debugging } = require('./Debugging.js');
  *
  * @return {object}      - The font object of that file
  */
-const GetFont = ( font ) => {
-	Debugging.report( `Running GetFont`, 1 );
+const GetFont = (font) => {
+	Debugging.report(`Running GetFont`, 1);
 
 	// try loading the font file
 	try {
-		let FONTFACE = require(`../fonts/${ font }.json`); // read font file
-
-		Debugging.report( `GetFont: Fontface path selected: "${ font }.json"`, 2 );
-
+		let FONTFACE = FONT_HOLSTER[font]; // read font file
+		Debugging.report(`GetFont: Fontface path selected: "${font}.json"`, 2);
+		if (!FONTFACE) return false;
 		return FONTFACE;
-	}
-	catch( error ) {
-		Debugging.error( `Font file for "${ font }" errored out: ${ error }`, 2 );
+	} catch (error) {
+		Debugging.error(`Font file for "${font}" errored out: ${error}`, 2);
 
 		return false;
 	}
 };
-
 
 module.exports = exports = {
 	GetFont,
